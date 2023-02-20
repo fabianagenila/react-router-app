e6ef1767b8ce6fa2746e91e91eb39ff2f5345560
fabianagenila <fabianagenila@gmail.com>
2023-02-20 18:29:59 -0300

    dynamic data loading

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hook/useFetch";
 
 const Products = () => {
   const { id } = useParams();
+  const url = "http://localhost:3000/products/" + id;
+  const { data: product, loading, error } = useFetch(url);
   return (
     <>
       <p>ID do Produto: {id}</p>
+      {error && <p>Ocorreu um erro!</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <>
+          <h1>{product.name}</h1>
+          <p>R$ {product.price}</p>
+        </>
+      )}
     </>
   );
 };
